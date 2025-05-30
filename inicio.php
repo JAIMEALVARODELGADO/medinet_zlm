977740ace8d0d39ebfd727b44e254da9531c5e2a
Jaime.Delgado <jaime.delgado@katary.co>
2025-05-29 22:26:30 -0500

    Se cambia titulo en el encabezado, de acuerdo a la parametrizacion
    Se cambia titulo en el encabezado, de acuerdo a la parametrizacion

--- inicio.php
+++ inicio.php
@@ -14,10 +14,34 @@ if(isset($_POST['id_persona'])){
 require_once "clases/conexion.php";
 $obj=new conectar();
 $conexion=$obj->conexion();
-$consenti="SELECT nombre_ent FROM entidad WHERE id_ent='1'";
-$consenti=mysqli_query($conexion,$consenti);
-$row=mysqli_fetch_row($consenti);
-$_SESSION['gnombre_ent']=$row[0];
+
+//Aqui consulto el nombre de la entidad para mostrarlo en el encabezado, de acuerdo a la parametrizacion
+$consparametros="SELECT estado FROM parametros_generales WHERE nombre_parametro ='nombreentidadxprofesional'";
+$consparametros=mysqli_query($conexion,$consparametros);
+if(mysqli_num_rows($consparametros) > 0) {    
+    $rowparametro=mysqli_fetch_array($consparametros);
+    $estadoparametro=$rowparametro['estado'];
+}
+else {
+    $estadoparametro='IN'; // Si no existe el parametro, lo dejo en estado inactivo
+}
+
+if($estadoparametro == 'AC'){
+    $consenti="SELECT CONCAT(pnom_per,' ',snom_per,' ',pape_per,' ',sape_per) as nombre
+    FROM persona p
+    WHERE p.id_persona ='{$_POST['id_persona']}'";
+    $consenti=mysqli_query($conexion,$consenti);
+    $row=mysqli_fetch_array($consenti);
+    $_SESSION['gnombre_ent']=$row['nombre'];
+} else {
+    $consenti="SELECT nombre_ent FROM entidad WHERE id_ent='1'";
+    $consenti=mysqli_query($conexion,$consenti);
+    $row=mysqli_fetch_row($consenti);
+    $_SESSION['gnombre_ent']=$row[0];
+}
+
+
+
 ?>
 <!DOCTYPE html>
 <html lang="es">
