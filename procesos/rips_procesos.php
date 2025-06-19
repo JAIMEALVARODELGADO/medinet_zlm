293116a30be2ac7886148e57abe61ba76c054cbe
Jaime.Delgado <jaime.delgado@katary.co>
2025-06-18 23:44:13 -0500

    Se crea rips US
    Se crea rips US

--- procesos/rips_procesos.php
+++ procesos/rips_procesos.php
@@ -6,6 +6,9 @@ switch($_GET['opcion']) {
     case 'crearRips':
         crearRips($_GET['id_factura']);
         break;
+    case 'traerUs':        
+        echo traerUs($_GET['id_factura']);
+        break;
     case 'rips':
         include 'procesos/rips_rips.php';
         break;
@@ -34,48 +37,270 @@ function crearRips($id_factura) {
     $row=mysqli_query($conexion,$sql);
     $resultado=mysqli_fetch_array($row);
     if($resultado['cantidad'] == 0) {
+        // Insertar datos en la tabla nrusuario
         $sql = "INSERT INTO nrusuario (
-tipo_documento,numdocumento,
-tipousuario,
-fechanacimiento,
-codsexo,
-codpaisresidencia,
-codmunicipioresidencia,
-codzonaresidencia,
-incapacidad,
-codpaisorigen,
-id_factura
-)
-        VALUES ()";
-        echo $sql;
-        //mysqli_query($conexion,$sql);
-    }
+        tipo_documento,numdocumento,tipousuario,fechanacimiento,
+        codsexo,codpaisresidencia,codmunicipioresidencia,codzonaresidencia,
+        incapacidad,codpaisorigen,id_factura)
+        SELECT dg.valor_det as tipo_documento,numero_iden_per,
+        tp.valor_det as tipo_usuario,fnac_per,sx.valor_det as sexo_per,170 as codpaisresidencia,codigo_mun,
+        CASE 
+            WHEN zona = 'R' THEN '01'
+            WHEN zona = 'U' THEN '02'
+            ELSE NULL
+        END AS zona,
+        'NO' as incapacidad,'170' as codpaisorigen,fe.id_factura
+        FROM factura_encabezado fe
+        inner join persona p on p.id_persona = fe.id_persona 
+        left join detalle_grupo dg on dg.codi_det = p.tipo_iden_per
+        left join paciente pa on pa.id_persona = p.id_persona
+        left join detalle_grupo tp on tp.codi_det = pa.tipo_usuario
+        left join detalle_grupo sx on sx.codi_det = p.sexo_per
+        WHERE fe.id_factura ='$id_factura'";
+        //echo $sql;
+        mysqli_query($conexion,$sql);
 
-
-
-
-
-	/*	$sql="INSERT INTO eps (codigo_eps,nit_eps,nombre_eps,direccion_eps,telefono_eps,contacto_eps)
-		VALUES ('$datos[0]','$datos[1]','$datos[2]','$datos[3]','$datos[4]','$datos[5]')";
-		return mysqli_query($conexion,$sql);
-
-        $obj=new conectar();
-		$conexion=$obj->conexion();
-
-		$sql="SELECT id_eps,codigo_eps, nit_eps, nombre_eps, direccion_eps, telefono_eps, contacto_eps FROM eps WHERE id_eps='$ideps'";
-		$row=mysqli_query($conexion,$sql);
-		$ver=mysqli_fetch_row($row);
-		$datos=array(
-			'id_eps' => $ver[0],
-			'codigo_eps' => $ver[1], 
-			'nit_eps' => $ver[2], 
-			'nombre_eps' => $ver[3], 
-			'direccion_eps' => $ver[4], 
-			'telefono_eps' => $ver[5], 
-			'contacto_eps' => $ver[6]
-			);
-		return $datos;*/
-
+        // Insertar datos en la tabla nrconsulta
+        $sql= "SELECT fe.fechaini_fac,'' as numautorizacion,
+        cd.codigo_cdet,'' as modalidadgruposervicio,'' as gruposervicio,        
+        '' as codservicio,'' as finalidad,'' as causamotivoatencion,'' as coddiagnosticoprincipal,
+        '' as cddiagnosticorelacionado1,'' as coddiagnosticorelacionado2,
+        '' as coddiagnosticorelacionado3,'' as tipodiagnosticoprincipal,
+        fd.valor_unit_detfac,'' as conceptorecaudo,'0' as valorpagomoderador,        
+        '' as numfevpagomoderador,fe.id_factura ,fd.id_detfac,fd.cantidad_detfac
+        FROM factura_detalle fd 
+        INNER JOIN factura_encabezado fe on fe.id_factura = fd.id_factura 
+        INNER JOIN convenio_detalle cd on cd.id_cdet = fd.id_cdet 
+        WHERE cd.tipo_cdet='AC' AND fd.id_factura ='$id_factura'";
+        $row = mysqli_query($conexion,$sql);
+        if(mysqli_num_rows($row) <> 0) {
+            $consecutivo = 1;
+            while($fila = mysqli_fetch_array($row)) {
+                // Preparar los datos para la inserción
     
+                $cantidad = $fila['cantidad_detfac'];
+                for($i=0; $i < $cantidad; $i++) {
+                    $sql = "INSERT INTO nrconsulta (
+                        fechainicioatencion,
+                        numautorizacion,
+                        codconsulta,
+                        modalidadgruposervicio,
+                        gruposervicio,
+                        codservicio,
+                        finalidadtecnologiasalud,
+                        causamotivoatencion,
+                        coddiagnosticoprincipal,
+                        coddiagnosticorelacionado1,
+                        coddiagnosticorelacionado2,
+                        coddiagnosticorelacionado3,
+                        tipodiagnosticoprincipal,
+                        vrservicio,
+                        conceptorecaudo,
+                        valorpagomoderador,
+                        numfevpagomoderador,
+                        consecutivo,
+                        id_factura,
+                        id_detalle)
+                        VALUES (
+                        '{$fila['fechaini_fac']}',
+                        '{$fila['numautorizacion']}',
+                        '{$fila['codigo_cdet']}',
+                        '{$fila['modalidadgruposervicio']}',
+                        '{$fila['gruposervicio']}',
+                        '{$fila['codservicio']}',
+                        '{$fila['finalidad']}',
+                        '{$fila['causamotivoatencion']}',
+                        '{$fila['coddiagnosticoprincipal']}',
+                        '{$fila['cddiagnosticorelacionado1']}',
+                        '{$fila['coddiagnosticorelacionado2']}',
+                        '{$fila['coddiagnosticorelacionado3']}',
+                        '{$fila['tipodiagnosticoprincipal']}',
+                        '{$fila['valor_unit_detfac']}',
+                        '{$fila['conceptorecaudo']}',
+                        '{$fila['valorpagomoderador']}',
+                        '{$fila['numfevpagomoderador']}',
+                        '$consecutivo',
+                        '{$fila['id_factura']}',
+                        '{$fila['id_detfac']}'                    
+                        )";
+                    //echo $sql;
+                    $consecutivo++;
+                    mysqli_query($conexion,$sql);                    
+                }    
+            }            
+        }
+        // Insertar datos en la tabla nrprocedimientos
+        $sql = "SELECT fe.fechaini_fac,'' as idmipres,'' as numautorizacion,
+        cd.codigo_cdet,'' as codprocedimiento,'' as viaingresoserviciosalud,
+        '' as modalidadgruposerviciotecsal,'' as gruposervicios,
+        '' as codservicio,'' as finalidadtecnologiasalud,
+        '' as coddiagnosticoprincipal,'' as coddiagnosticorelacionado,
+        '' as codcomplicacion,fd.valor_unit_detfac,'' as conceptorecaudo,
+        '0' as valorpagomoderador,'' as numfevpagomoderador,
+        fe.id_factura ,fd.id_detfac,fd.cantidad_detfac
+        FROM factura_detalle fd 
+        INNER JOIN factura_encabezado fe on fe.id_factura = fd.id_factura 
+        INNER JOIN convenio_detalle cd on cd.id_cdet = fd.id_cdet 
+        WHERE cd.tipo_cdet='AP' AND fd.id_factura ='$id_factura'";
+        //echo $sql;
+        $row = mysqli_query($conexion,$sql);
+        if(mysqli_num_rows($row) <> 0) {
+            $consecutivo = 1;
+            while($fila = mysqli_fetch_array($row)) {
+                // Preparar los datos para la inserción
+    
+                $cantidad = $fila['cantidad_detfac'];
+                for($i=0; $i < $cantidad; $i++) {
+                    $sql = "INSERT INTO nrprocedimientos(
+                        fechainicioatencion,
+                        idmipres,
+                        numautorizacion,
+                        codprocedimiento,
+                        viaingresoserviciosalud,
+                        modalidadgruposerviciotecsal,
+                        gruposervicios,
+                        codservicio,
+                        finalidadtecnologiasalud,                        
+                        coddiagnosticoprincipal,
+                        coddiagnosticorelacionado,
+                        codcomplicacion,
+                        vrservicio,
+                        conceptorecaudo,
+                        valorpagomoderador,
+                        numfevpagomoderador,
+                        consecutivo,
+                        id_factura,
+                        id_detfac)
+                        VALUES (
+                        '{$fila['fechaini_fac']}',
+                        '{$fila['idmipres']}',
+                        '{$fila['numautorizacion']}',                        
+                        '{$fila['codprocedimiento']}',
+                        '{$fila['viaingresoserviciosalud']}',
+                        '{$fila['modalidadgruposerviciotecsal']}',
+                        '{$fila['gruposervicios']}',
+                        '{$fila['codservicio']}',
+                        '{$fila['finalidadtecnologiasalud']}',
+                        '{$fila['coddiagnosticoprincipal']}',
+                        '{$fila['coddiagnosticorelacionado']}',
+                        '{$fila['codcomplicacion']}',
+                        '{$fila['valor_unit_detfac']}',
+                        '{$fila['conceptorecaudo']}',
+                        '{$fila['valorpagomoderador']}',
+                        '{$fila['numfevpagomoderador']}',
+                        '$consecutivo',
+                        '{$fila['id_factura']}',
+                        '{$fila['id_detfac']}'                        
+                        )";
+                    //echo "<br>".$sql;
+                    $consecutivo++;
+                    mysqli_query($conexion,$sql);                    
+                }    
+            }
+        }
+
+        // Insertar datos en la tabla nrotroservicios
+        $sql = "SELECT '' as numautorizacion,0 as idmipres,fe.fechaini_fac,
+        '' as tipoos,cd.codigo_cdet,cd.descripcion_cdet as nomtecnologia,
+        fd.cantidad_detfac as cantidados, fd.valor_unit_detfac as vrunitos,
+        fd.cantidad_detfac * fd.valor_unit_detfac as vrservicio,
+        '' as conceptorecaudo,'0' as valorpagomoderador,
+        '' as numfevpagomoderador,fe.id_factura ,fd.id_detfac
+        FROM factura_detalle fd 
+        INNER JOIN factura_encabezado fe on fe.id_factura = fd.id_factura 
+        INNER JOIN convenio_detalle cd on cd.id_cdet = fd.id_cdet 
+        WHERE cd.tipo_cdet='AT' AND fd.id_factura ='$id_factura'";
+        //echo $sql;
+        $row = mysqli_query($conexion,$sql);
+        if(mysqli_num_rows($row) <> 0) {
+            $consecutivo = 1;
+            while($fila = mysqli_fetch_array($row)) {
+                // Preparar los datos para la inserción
+                $sql = "INSERT INTO nrotroservicios(
+                        numautorizacion,idmipres,fechasuministrotecnologia,
+                        tipoos,codtecnologia,nomtecnologia,cantidados,vrunitos,
+                        vrservicio,conceptorecaudo,valorpagomoderador,
+                        numfevpagomoderador,consecutivo,id_factura,id_detalle)
+                        VALUES (
+                        '{$fila['numautorizacion']}',
+                        '{$fila['idmipres']}',
+                        '{$fila['fechaini_fac']}',
+                        '{$fila['tipoos']}',
+                        '{$fila['codigo_cdet']}',
+                        '{$fila['nomtecnologia']}',
+                        '{$fila['cantidados']}',
+                        '{$fila['vrunitos']}',
+                        '{$fila['vrservicio']}',
+                        '{$fila['conceptorecaudo']}',
+                        '{$fila['valorpagomoderador']}',
+                        '{$fila['numfevpagomoderador']}',
+                        '$consecutivo',
+                        '{$fila['id_factura']}',
+                        '{$fila['id_detfac']}'
+                        )";                        
+                //echo "<br>".$sql;
+                $consecutivo++;
+                mysqli_query($conexion,$sql);
+            }
+        }
+    }
+}
+
+function traerUs($id_factura) {
+    $obj=new conectar();
+    $conexion=$obj->conexion();
+
+    $sql = "SELECT id_usuario, tipo_documento, numdocumento, tipousuario, fechanacimiento, codsexo, codpaisresidencia, codmunicipioresidencia, codzonaresidencia, incapacidad, codpaisorigen,
+        CONCAT (p.pnom_per, ' ', p.snom_per,' ',p.pape_per,' ',p.sape_per) as nombre_completo
+        FROM nrusuario nru
+        inner join factura_encabezado fe on fe.id_factura = nru.id_factura 
+        inner join persona p on p.id_persona = fe.id_persona 
+        WHERE nru.id_factura = '$id_factura'";
+    //echo $sql;
+    
+    $result = mysqli_query($conexion,$sql);
+    $data = array();
+    
+    while($row = mysqli_fetch_assoc($result)) {
+        $data[] = $row;
+    }
+    //echo json_encode($data);
+    return(json_encode($data));
+}
+
+function actualizarUsuario($datos) {
+    $obj = new conectar();
+    $conexion = $obj->conexion();
+    
+    $id_usuario = mysqli_real_escape_string($conexion, $datos['id_usuario']);
+    $tipo_documento = mysqli_real_escape_string($conexion, $datos['tipo_documento']);
+    $numdocumento = mysqli_real_escape_string($conexion, $datos['numdocumento']);
+    $tipousuario = mysqli_real_escape_string($conexion, $datos['tipousuario']);
+    $fechanacimiento = mysqli_real_escape_string($conexion, $datos['fechanacimiento']);
+    $codsexo = mysqli_real_escape_string($conexion, $datos['codsexo']);
+    $codpaisresidencia = mysqli_real_escape_string($conexion, $datos['codpaisresidencia']);
+    $codmunicipioresidencia = mysqli_real_escape_string($conexion, $datos['codmunicipioresidencia']);
+    $codzonaresidencia = mysqli_real_escape_string($conexion, $datos['codzonaresidencia']);
+    $incapacidad = mysqli_real_escape_string($conexion, $datos['incapacidad']);
+    $codpaisorigen = mysqli_real_escape_string($conexion, $datos['codpaisorigen']);
+    
+    $sql = "UPDATE nrusuario SET 
+            tipo_documento = '$tipo_documento',
+            numdocumento = '$numdocumento',
+            tipousuario = '$tipousuario',
+            fechanacimiento = '$fechanacimiento',
+            codsexo = '$codsexo',
+            codpaisresidencia = '$codpaisresidencia',
+            codmunicipioresidencia = '$codmunicipioresidencia',
+            codzonaresidencia = '$codzonaresidencia',
+            incapacidad = '$incapacidad',
+            codpaisorigen = '$codpaisorigen'
+            WHERE id_usuario = '$id_usuario'";
+    
+    if(mysqli_query($conexion, $sql)) {
+        echo "Usuario actualizado correctamente";
+    } else {
+        echo "Error al actualizar usuario: " . mysqli_error($conexion);
+    }
 }
 ?>
--- tablaRipsUs.php
+++ tablaRipsUs.php
@@ -17,27 +17,133 @@ $id_factura = $_GET['id_factura'];
 	function crearRips() {
 		var url = "procesos/rips_procesos.php?id_factura=" + id_factura
 		+"&opcion=crearRips";
-		console.log(url);
-		fetch(url, {
+		//console.log(url);
+		fetchOptions={
 			method: 'GET',
 			headers: {
 				'Content-Type': 'application/x-www-form-urlencoded'
 			}
-		})
+		}
+		fetch(url, fetchOptions)
 		.then(response => response.text())
-		.then(data => {
-			//document.getElementById('resultado').innerHTML = data;
-			cargarTablaRips(data);
+		.then(data => {			
+			cargarUs();
 		})
 		.catch(error => {
 			console.error('Error:', error);
 		});
 	}
 
-	function cargarTablaRips(data) {
-		alert(data);
-		//document.getElementById('tablaDataRips').innerHTML = data;
-		// Aquí puedes agregar más lógica si es necesario
+	function cargarUs() {
+		var url = "procesos/rips_procesos.php?id_factura=" + id_factura
+		+"&opcion=traerUs";
+		console.log(url);
+		fetchOptions={
+			method: 'GET',
+			headers: {				
+				'Content-Type': 'application/json' 
+			}
+		}
+		fetch(url, fetchOptions)		
+		.then(response => response.json())
+		.then(data => {			
+            mostrarUs(data);
+		})
+		.catch(error => {
+			console.error('Error:', error);
+		});
+	}
+
+	function mostrarUs(usuarios){
+        // Limpiar el tbody de la tabla
+        $('#tablaRipsUs tbody').empty();
+        
+        // Verificar si hay datos
+        if (!usuarios || usuarios.length === 0) {
+            $('#tablaRipsUs tbody').append('<tr><td colspan="9" class="text-center">No hay usuarios para mostrar</td></tr>');
+            return;
+        }
+        
+        // Recorrer los usuarios y crear las filas
+        usuarios.forEach(function(usuario) {
+            var fila = '<tr>' +
+                '<td>' + (usuario.tipo_documento || '') + '</td>' +
+                '<td>' + (usuario.numdocumento || '') + '</td>' +
+                '<td>' + (usuario.nombre_completo || 'N/A') + '</td>' + // Nota: este campo no viene en tu JSON actual
+                '<td>' + (usuario.tipousuario || '') + '</td>' +
+                '<td>' + (usuario.fechanacimiento || '') + '</td>' +
+                '<td>' + (usuario.codsexo || '') + '</td>' +
+                '<td>' +
+				'<span class="btn btn-warning btn.sm" data-toggle="modal" data-target="#modalEditar" title="Editar" onclick="editarUs()">'+
+					'<span class="far fa-edit"></span>'+
+				'</span>' +
+				'</td>' +
+				'<td>' +				                
+                '</tr>';
+            
+            $('#tablaRipsUs tbody').append(fila);
+        });
+    }
+
+	function editarUs(id_usuario) {
+		// Cargar los datos del usuario específico
+		var url = "procesos/rips_procesos.php?id_usuario=" + id_usuario + "&opcion=traerUsuario";
+		
+		fetch(url)
+		.then(response => response.json())
+		.then(data => {
+			if(data && data.length > 0) {
+				var usuario = data[0];
+				// Llenar el formulario del modal con los datos
+				$('#edit_id_usuario').val(usuario.id_usuario);
+				$('#edit_tipo_documento').val(usuario.tipo_documento);
+				$('#edit_numdocumento').val(usuario.numdocumento);
+				$('#edit_tipousuario').val(usuario.tipousuario);
+				$('#edit_fechanacimiento').val(usuario.fechanacimiento);
+				$('#edit_codsexo').val(usuario.codsexo);
+				$('#edit_codpaisresidencia').val(usuario.codpaisresidencia);
+				$('#edit_codmunicipioresidencia').val(usuario.codmunicipioresidencia);
+				$('#edit_codzonaresidencia').val(usuario.codzonaresidencia);
+				$('#edit_incapacidad').val(usuario.incapacidad);
+				$('#edit_codpaisorigen').val(usuario.codpaisorigen);
+			}
+		})
+		.catch(error => {
+			console.error('Error al cargar usuario:', error);
+			alert('Error al cargar los datos del usuario');
+		});
+	}
+
+	function guardarUsuario() {
+		// Recopilar los datos del formulario
+		var formData = new FormData();
+		formData.append('opcion', 'actualizarUsuario');
+		formData.append('id_usuario', $('#edit_id_usuario').val());
+		formData.append('tipo_documento', $('#edit_tipo_documento').val());
+		formData.append('numdocumento', $('#edit_numdocumento').val());
+		formData.append('tipousuario', $('#edit_tipousuario').val());
+		formData.append('fechanacimiento', $('#edit_fechanacimiento').val());
+		formData.append('codsexo', $('#edit_codsexo').val());
+		formData.append('codpaisresidencia', $('#edit_codpaisresidencia').val());
+		formData.append('codmunicipioresidencia', $('#edit_codmunicipioresidencia').val());
+		formData.append('codzonaresidencia', $('#edit_codzonaresidencia').val());
+		formData.append('incapacidad', $('#edit_incapacidad').val());
+		formData.append('codpaisorigen', $('#edit_codpaisorigen').val());
+
+		fetch('procesos/rips_procesos.php', {
+			method: 'POST',
+			body: formData
+		})
+		.then(response => response.text())
+		.then(data => {
+			alert('Usuario actualizado correctamente');
+			$('#modalEditar').modal('hide');
+			cargarUs(); // Recargar la tabla
+		})
+		.catch(error => {
+			console.error('Error:', error);
+			alert('Error al actualizar el usuario');
+		});
 	}
 </script>
 
@@ -72,11 +178,25 @@ $id_factura = $_GET['id_factura'];
 	                <div class="card text-left">
 	                    
 	                    <div class="card-body">
+							<table class="table table-hover table-sm table-bordered font13" id="tablaRipsUs">
+								<thead style="background-color: #2574a9;color: white; font-weight: bold;">
+									<tr>				
+										<td>Tp Doc</td>
+										<td>Número</td>
+										<td>Nombre</td>
+										<td>Tp Usuario</td>
+										<td>Fecha Nac</td>
+										<td>Sexo</td>
+										<td colspan="3">Opciones</td>				
+									</tr>
+								</thead>
+								
+								<tbody style="background-color: white">
+									
+								</tbody>
+								
+							</table>
 
-                            
-
-
-	                        
 	                        <!--<div id="tablaDatafactura"></div>-->
 	                    </div>
 	                    
@@ -86,4 +206,126 @@ $id_factura = $_GET['id_factura'];
 	    </div>		
 	</div>
 
-	
+<!-- Modal para Editar Usuario -->
+<div class="modal fade" id="modalEditar" tabindex="-1" role="dialog" aria-labelledby="modalEditarLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modalEditarLabel">Editar Usuario RIPS</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form id="formEditarUsuario">
+                    <input type="hidden" id="edit_id_usuario" name="id_usuario">
+                    
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="edit_tipo_documento">Tipo Documento:</label>
+                                <select class="form-control" id="edit_tipo_documento" name="tipo_documento" required>
+                                    <option value="">Seleccione...</option>
+                                    <option value="CC">Cédula de Ciudadanía</option>
+                                    <option value="TI">Tarjeta de Identidad</option>
+                                    <option value="RC">Registro Civil</option>
+                                    <option value="PA">Pasaporte</option>
+                                    <option value="CE">Cédula de Extranjería</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="edit_numdocumento">Número Documento:</label>
+                                <input type="text" class="form-control" id="edit_numdocumento" name="numdocumento" maxlength="20" required>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="edit_tipousuario">Tipo Usuario:</label>
+                                <select class="form-control" id="edit_tipousuario" name="tipousuario" required>
+                                    <option value="">Seleccione...</option>
+                                    <option value="01">Contributivo</option>
+                                    <option value="02">Subsidiado</option>
+                                    <option value="03">Vinculado</option>
+                                    <option value="04">Particular</option>
+                                    <option value="05">Otro</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="edit_fechanacimiento">Fecha Nacimiento:</label>
+                                <input type="date" class="form-control" id="edit_fechanacimiento" name="fechanacimiento" required>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="edit_codsexo">Sexo:</label>
+                                <select class="form-control" id="edit_codsexo" name="codsexo" required>
+                                    <option value="">Seleccione...</option>
+                                    <option value="M">Masculino</option>
+                                    <option value="F">Femenino</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="edit_codpaisresidencia">Código País Residencia:</label>
+                                <input type="text" class="form-control" id="edit_codpaisresidencia" name="codpaisresidencia" maxlength="3" value="170" required>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="edit_codmunicipioresidencia">Código Municipio:</label>
+                                <input type="text" class="form-control" id="edit_codmunicipioresidencia" name="codmunicipioresidencia" maxlength="5" required>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="edit_codzonaresidencia">Zona Residencia:</label>
+                                <select class="form-control" id="edit_codzonaresidencia" name="codzonaresidencia" required>
+                                    <option value="">Seleccione...</option>
+                                    <option value="01">Rural</option>
+                                    <option value="02">Urbana</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="edit_incapacidad">Incapacidad:</label>
+                                <select class="form-control" id="edit_incapacidad" name="incapacidad" required>
+                                    <option value="">Seleccione...</option>
+                                    <option value="NO">No</option>
+                                    <option value="SI">Sí</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="edit_codpaisorigen">Código País Origen:</label>
+                                <input type="text" class="form-control" id="edit_codpaisorigen" name="codpaisorigen" maxlength="3" value="170" required>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                <button type="button" class="btn btn-primary" onclick="guardarUsuario()">Guardar Cambios</button>
+            </div>
+        </div>
+    </div>
+</div>
